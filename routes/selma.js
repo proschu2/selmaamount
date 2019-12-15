a7760246f58b4664aada712a9a66ec8c904648aa
Sanzio Monti <github@sanziomonti.xyz>
2019-12-15 12:36:43 +0100

    remove procfile

--- routes/selma.js
+++ routes/selma.js
@@ -46,12 +46,20 @@ const selma = async () => {
 
   await navigationPromise
 
+  await page.waitForSelector('.my-planet-hero > .my-planet-stats > .my-planet-hero-top-row > .column > .stat-figure')
+  await page.click('.my-planet-hero > .my-planet-stats > .my-planet-hero-top-row > .column > .stat-figure')
+  let element = await page.$('.my-planet-hero > .my-planet-stats > .my-planet-hero-top-row > .column > .stat-figure');
+  var amount = await page.evaluate(element => element.textContent, element);
+  amount = amount.replace('CHF', '');
+  amount = amount.replace("'", '');
+  amount = amount.replace('’','');
+
   await page.waitForSelector('.my-planet-stats > .my-planet-hero-top-row > .column > .change-figures > .change-figure:nth-child(2)')
   await page.click('.my-planet-stats > .my-planet-hero-top-row > .column > .change-figures > .change-figure:nth-child(2)')
-  let element = await page.$('.my-planet-stats > .my-planet-hero-top-row > .column > .change-figures > .change-figure:nth-child(2)');
-  var amount = await page.evaluate(element => element.textContent, element);
-  amount = amount.replace('+', '');
-  amount = amount.replace('CHF', '');
+  element = await page.$('.my-planet-stats > .my-planet-hero-top-row > .column > .change-figures > .change-figure:nth-child(2)');
+  var change = await page.evaluate(element => element.textContent, element);
+  change = change.replace('+', '');
+  change = change.replace('CHF', '');
 
 
   await page.waitForSelector('.my-planet-stats > .my-planet-hero-top-row > .column > .change-figures > .change-figure:nth-child(4)')
@@ -62,19 +70,19 @@ const selma = async () => {
   perc = perc.replace('%', '');
   await browser.close()
 
-  return [amount.trim(), perc]
+  return [amount.trim(), perc, change.trim()]
 }
 
 router.get('/', async function (req, res, ) {
   const selmaAmount = await selma();
-  res.send(`${selmaAmount[0]} | ${selmaAmount[1]}`);
+  res.send(`${selmaAmount[0]} | ${selmaAmount[2]} | ${selmaAmount[1]}`);
 });
 
 router.get('/add', async function (req, res, next) {
   const selmaAmount = await selma();
   try {
     const client = await pool.connect()
-    const result = await client.query(`INSERT INTO SELMA(date,amount,percentage) VALUES (now(),${Number(selmaAmount[0])},${Number(selmaAmount[1])});`);
+    const result = await client.query(`INSERT INTO SELMA(date,amount,percentage,change) VALUES (now(),${Number(selmaAmount[0])},${Number(selmaAmount[1])}, ${Number(selmaAmount[2])});`);
     const results = {
       'results': (result) ? result.rows : null
     };
